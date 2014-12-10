09bf1e28fd8b2fc79ccc0d74dfa7aa1b2b4f7579
Chris Erin <dev+chriserin@hashrocket.com>
2014-12-10 11:15:54 -0600

    Parse git diff

---
+++ lib/finland/git_diff.rb
@@ -0,0 +1,29 @@
+module Finland
+  class GitDiff
+    def self.get(commits_number)
+      `git diff -U0 HEAD~#{commits_number}`
+    end
+
+    def self.parse(diff_txt)
+      reduced_lines = diff_txt.lines.reject {|x| x.match /^[-+] /}
+      reduced_lines = reduced_lines.reject {|x| x.match /^index|\+\+\+/ }
+
+      current_file = ""
+      reduced_lines.each_with_object({}) do |line, result|
+        if match = line.match(/^--- a\/(.*)$/)
+          current_file = match[1]
+          result[current_file] = []
+        elsif match = line.match(/^@@/)
+          result[current_file] << parse_line_diff(line)
+        end
+      end
+    end
+
+    def self.parse_line_diff(line)
+      match = line.match /^@@ -(?<line_number>\d+),?(?<line_quantity>\d+)? \+\d/
+      number = match["line_number"].to_i
+      quantity = match["line_quantity"].to_i
+      number..(number + [quantity - 1, 0].max)
+    end
+  end
+end
---
+++ spec/git_diff_spec.rb
@@ -0,0 +1,79 @@
+require 'finland/git_diff'
+
+describe Finland::GitDiff do
+  it 'should parse git diff' do
+    git_diff = <<-DIFF
+diff --git a/db/seeds/development.rb b/db/seeds/development.rb
+index 8843d8f..fac62bf 100644
+--- a/db/seeds/development.rb
++++ b/db/seeds/development.rb
+@@ -155,2 +155 @@ booking = Booking.find_or_create_by!(seller_id: seller.id, customer_id: customer
+-  booking.parking_start = 20.days.from_now
+-  booking.parking_end   = 20.days.from_now + 3.hours
++  booking.parking_period = (20.days.from_now..(20.days.from_now + 3.hours))
+    DIFF
+
+    result = Finland::GitDiff.parse(git_diff)
+    expect(result).to eq({"db/seeds/development.rb" => [155..156]})
+  end
+
+  it 'should parse git diff with multiple diffs' do
+    git_diff = <<-DIFF
+diff --git a/db/seeds/development.rb b/db/seeds/development.rb
+index 8843d8f..fac62bf 100644
+--- a/db/seeds/development.rb
++++ b/db/seeds/development.rb
+@@ -155,2 +155 @@ booking = Booking.find_or_create_by!(seller_id: seller.id, customer_id: customer
+-  booking.parking_start = 20.days.from_now
+-  booking.parking_end   = 20.days.from_now + 3.hours
++  booking.parking_period = (20.days.from_now..(20.days.from_now + 3.hours))
+@@ -174,2 +173 @@ booking = Booking.find_or_create_by!(seller_id: seller.id, customer_id: customer
+-  booking.parking_start = 22.days.from_now
+-  booking.parking_end   = 22.days.from_now + 3.hours
++  booking.parking_period = (22.days.from_now..(22.days.from_now + 3.hours))
+@@ -192,2 +190 @@ booking_yesterday = Booking.find_or_create_by!(seller_id: seller.id, customer_id
+-  booking.parking_start = 2.days.ago
+-  booking.parking_end   = 2.days.ago + 3.hours
++  booking.parking_period = (2.days.from_now..(2.days.from_now + 3.hours))
+@@ -210,2 +207 @@ booking_in_past = Booking.find_or_create_by!(seller_id: seller.id, customer_id:
+-  booking.parking_start = 4.days.ago
+-  booking.parking_end   = 4.days.ago + 3.hours
++  booking.parking_period = (4.days.from_now..(4.days.from_now + 3.hours))
+    DIFF
+
+    result = Finland::GitDiff.parse(git_diff)
+    expect(result).to eq({"db/seeds/development.rb" => [155..156, 174..175, 192..193, 210..211]})
+  end
+
+  it 'should parse git diff with multiple files' do
+    git_diff = <<-DIFF
+diff --git a/app/models/booking.rb b/app/models/booking.rb
+index 09ad575..09e485b 100644
+--- a/app/models/booking.rb
++++ b/app/models/booking.rb
+@@ -108 +108 @@ class Booking < ActiveRecord::Base
+-    parking_period.first
++    parking_period.first.in_time_zone Time.zone
+@@ -112 +112 @@ class Booking < ActiveRecord::Base
+-    parking_period.last
++    parking_period.last.in_time_zone Time.zone
+diff --git a/app/models/cancellation.rb b/app/models/cancellation.rb
+index 19749b1..4b61910 100644
+--- a/app/models/cancellation.rb
++++ b/app/models/cancellation.rb
+@@ -34,2 +34,2 @@ class Cancellation < ActiveRecord::Base
+-      event_date: Date.today,
+-      post_date: Date.today,
++      event_time: Time.zone.now,
++      post_time: Time.zone.now,
+@@ -43,2 +43,2 @@ class Cancellation < ActiveRecord::Base
+-      event_date: Date.today,
+-      post_date: Date.today,
++      event_time: Time.zone.now,
++      post_time: Time.zone.now,
+    DIFF
+
+    result = Finland::GitDiff.parse(git_diff)
+    expect(result).to eq({"app/models/booking.rb" => [108..108, 112..112], "app/models/cancellation.rb" => [34..35, 43..44]})
+  end
+end
