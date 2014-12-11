981a2dbe5630ebd12515684d51ff51bf9f50c7d2
Chris Erin and Jake Worth <dev+chriserin+jwworth@hashrocket.com>
2014-12-11 16:51:11 -0600

    Use affected tests method on finland

--- bin/finland.rb
+++ bin/finland.rb
@@ -8,6 +8,4 @@ require 'finland'
 
 Finland.index_location = "index.txt"
 
-diff = Finland::GitDiff.parse(Finland::GitDiff.get(1))
-index = Finland.load_index
-puts Finland::Compare.determine_affected_tests(index, diff)
+puts Finland.affected_tests()
